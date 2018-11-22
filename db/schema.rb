85a1519e8dde05c985d54d1b716e7acb3d5893fb
Anderson Min <andersonmingz@gmail.com>
2018-11-22 20:19:44 +0800

    Add user name show in navbar dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_045235) do
+ActiveRecord::Schema.define(version: 2018_11_22_084244) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2018_11_22_045235) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "article_id"
+    t.index ["article_id"], name: "index_users_on_article_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
