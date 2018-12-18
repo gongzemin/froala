cf60f731bd34a30740125e53572182510deb40d9
Anderson Min <andersonmingz@gmail.com>
2018-12-18 16:38:00 +0800

    Validation article attributes, show project goal

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,12 +5,17 @@ class Article < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
-
 	has_one_attached :project_image
 	has_one_attached :project_cover
 
-	validates :title, presence: true, length: { maximum: 110 }
-
     # Additionally can declare the category association as optional:
-	belongs_to :category, optional: true
+	# belongs_to :category, optional: true
+	belongs_to :category 
+
+	validates :title, presence: true, length: { maximum: 110 }
+	validates :subtitle, presence: true, length: { maximum: 210 }
+	validates :goal, presence: true
+	validates :project_cover, presence: true
+	validates :body, presence: true
 end
+
