01d85052d0d92b072f851a2d74e166afb035344c
Anderson Min <andersonmingz@gmail.com>
2018-12-19 20:17:07 +0800

    change jQuery order, put it first

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require jquery-ui
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
 //= require froala_editor.min.js
